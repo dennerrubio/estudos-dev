bbe97ce550daeba22d4b5027591065d3590ab1d1
Denner Rubio <denner.psd@gmail.com>
2023-03-21 16:29:03 -0300

    js ex01 att

--- javascript/curso-em-video/ex01/script.js
+++ javascript/curso-em-video/ex01/script.js
@@ -1,3 +1,10 @@
-window.alert('Olá Mundo!')
-window.confirm('Teste de mensagem com confirmação.')
-window.prompt('Escreva algum texto:')
+
+function getMessage() {
+    window.alert('Olá Mundo!')
+    window.confirm('Teste de mensagem com confirmação.')
+    window.prompt('Escreva algum texto:')
+}
+
+function mudarLuz() {
+    document.getElementById("luz").src="images/luz2.jpg"
+}
