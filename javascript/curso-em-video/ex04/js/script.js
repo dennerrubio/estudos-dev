2f128d0fa1f370099827edc04bbd8e9560ef271d
Denner Rubio <denner.psd@gmail.com>
2023-03-27 16:53:12 -0300

    js ex04 add

---
+++ javascript/curso-em-video/ex04/js/script.js
@@ -0,0 +1,31 @@
+var botao = document.querySelector("input#botao");
+var botao2 = document.querySelector("input#botao2");
+botao.addEventListener("click", testevelocidade);
+botao2.addEventListener("click", nacionalidade);
+
+function testevelocidade() {
+  var velocidade = document.getElementById("vel");
+  var resultado = document.getElementById("resultado");
+  var velocidade = Number(velocidade.value);
+  if (velocidade > 30) {
+    resultado.innerHTML =
+      "Tomou multa de graça, eu mandei você ficar de olho no radar.";
+  } else {
+    resultado.innerHTML = "Beleza, pode acelerar agora.";
+  }
+}
+
+function nacionalidade() {
+  var nacao = document.querySelector("input#pais").value;
+  var resultado2 = document.querySelector("p#resultado2");
+  if (
+    nacao == "Brasil" ||
+    nacao == "brasil" ||
+    nacao == "Brazil" ||
+    nacao == "brazil"
+  ) {
+    resultado2.innerHTML = "Estaaaaaaaamos no Brasil! PLIM PLIM";
+  } else {
+    resultado2.innerHTML = "Você é estrangeiro!";
+  }
+}
