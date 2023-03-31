0f7cfb897f2c49922999e48b888064600fd28d65
Denner Rubio <denner.psd@gmail.com>
2023-03-31 07:09:38 -0300

    js ex07 att

--- javascript/curso-em-video/exercicios/ex07/js/script.js
+++ javascript/curso-em-video/exercicios/ex07/js/script.js
@@ -5,12 +5,28 @@ function calcular() {
   var sexo = document.getElementsByName("radio");
   var resultado = document.getElementById("resultado");
   var genero = "";
-  
+  var idade = ano - anousuario;
+
   if (sexo[0].checked) {
     var genero = "Masculino";
+
+    if (anousuario > ano) {
+      window.alert(`${idade}, Viajante do tempo ${genero}`);
+    } else if (idade <= 6) {
+      
+    } else if (idade <= 18) {
+      window.alert(`${idade}, criança ${genero}`);
+    } else if (idade <= 59) {
+      window.alert(`${idade}, adulto ${genero}`);
+    } else if (idade <= 95) {
+      window.alert(`${idade}, velho ${genero}`);
+    } else if (idade >= 96) {
+      window.alert(`${idade}, titã velho ${genero}`);
+    }
+
   } else if (sexo[1].checked) {
     var genero = "Feminino";
   }
 
-  window.alert(`Ano atual ${ano}, vc nasceu ${anousuario} e é ${genero}`);
+  resultado.innerHTML = `Você é um(a) ${genero} com ${idade} anos de idade.`;
 }
