07fde915c643e43a2ea9ee4f3f7e6ff375feee1a
Denner Rubio <denner.psd@gmail.com>
2023-04-03 16:05:39 -0300

    js ex08 add

---
+++ javascript/curso-em-video/exercicios/ex08/for.js
@@ -0,0 +1,5 @@
+console.log("Inicio dos Testes");
+for (var a = 1; a <= 10; a++) {
+  console.log(`Teste de For nº${a}`);
+}
+console.log("Fim dos Testes");
---
+++ javascript/curso-em-video/exercicios/ex08/while.js
@@ -0,0 +1,7 @@
+console.log("Inicio do teste");
+var a = 1;
+while (a <= 10) {
+  console.log(`Texto rodando no while nº${a}`);
+  a++;
+}
+console.log("Fim do teste");
