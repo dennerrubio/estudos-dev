a56e1164e942d71656288081578f3641ff7936cd
Denner Rubio <denner.psd@gmail.com>
2023-04-08 10:14:48 -0300

    js ex11 add

---
+++ javascript/curso-em-video/exercicios/ex11/array.js
@@ -0,0 +1,7 @@
+console.log("teste")
+let numeros = [3, 41, 5, 6, 2, 53];
+console.log(numeros);
+console.log(numeros[2]);
+for (var c = 0 ; c < numeros.length ; c++) {
+    console.log(`O valor do slot ${c} é igual a ${numeros[c]}`);   
+}
