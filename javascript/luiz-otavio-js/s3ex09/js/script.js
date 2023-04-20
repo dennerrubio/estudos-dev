23b76f229fe0118caad48a6e692a81657d366ee6
Denner Rubio <denner.psd@gmail.com>
2023-04-20 16:19:38 -0300

    js s3 ex09 att

--- javascript/luiz-otavio-js/s3ex09/js/script.js
+++ javascript/luiz-otavio-js/s3ex09/js/script.js
@@ -1,14 +1,9 @@
 window.onload = function coletarData() {
   const caixa = document.querySelector("#caixa");
   const data = new Date();
-  const hora = data.getHours();
-  const minutos = data.getMinutes();
-  const minutosFix = "";
-  const horario = createClock(hora, minutos, minutosFix);
-  const dataNomeSemana = getDayName(data);
-  const dataNomeMes = getMonthName(data);
   const texto = criarTexto("texto");
-  texto.innerHTML = `${dataNomeSemana}, ${data.getDate()} de ${dataNomeMes} de ${data.getFullYear()} <br> ${horario}`;
+  texto.innerHTML = `${getDayName(data)}, ${data.getDate()} de 
+  ${getMonthName(data)} de ${data.getFullYear()} <br> ${createClock(data)}`;
   caixa.appendChild(texto);
 };
 
@@ -18,96 +13,47 @@ function criarTexto(className) {
   return p;
 }
 
-function createClock(hora, minutos, minutosFix) {
+function createClock(data) {
+  const hora = data.getHours();
+  const minutos = data.getMinutes();
+  let minutosFix = "";
   if (minutos <= 9) {
     minutosFix = "0";
   } else {
     minutosFix = "";
   }
-  const horario = `${hora}:${minutosFix}${minutos}`;
-  return horario;
+  return `${hora}:${minutosFix}${minutos}`;
 }
 
 function getDayName(data) {
   const dataSemana = data.getDay();
-  if (dataSemana === 0) {
-    const getDayName = "domingo";
-    return getDayName;
-  }
-  if (dataSemana === 1) {
-    const getDayName = "segunda-feira";
-    return getDayName;
-  }
-  if (dataSemana === 2) {
-    const getDayName = "terça-feira";
-    return getDayName;
-  }
-  if (dataSemana === 3) {
-    const getDayName = "quarta-feira";
-    return getDayName;
-  }
-  if (dataSemana === 4) {
-    const getDayName = "quinta-feira";
-    return getDayName;
-  }
-  if (dataSemana === 5) {
-    const getDayName = "sexta-feira";
-    return getDayName;
-  }
-  if (dataSemana === 6) {
-    const getDayName = "sábado";
-    return getDayName;
-  }
+  const diasSemanas = [
+    "domingo",
+    "segunda-feira",
+    "terça-feira",
+    "quarta-feira",
+    "quinta-feira",
+    "sexta-feira",
+    "sábado",
+  ];
+  return diasSemanas[dataSemana];
 }
 
 function getMonthName(data) {
   const dataMes = data.getMonth();
-  if (dataMes === 0) {
-    const getMonthName = "janeiro";
-    return getMonthName;
-  }
-  if (dataMes === 1) {
-    const getMonthName = "fevereiro";
-    return getMonthName;
-  }
-  if (dataMes === 2) {
-    const getMonthName = "março";
-    return getMonthName;
-  }
-  if (dataMes === 3) {
-    const getMonthName = "abril";
-    return getMonthName;
-  }
-  if (dataMes === 4) {
-    const getMonthName = "maio";
-    return getMonthName;
-  }
-  if (dataMes === 5) {
-    const getMonthName = "junho";
-    return getMonthName;
-  }
-  if (dataMes === 6) {
-    const getMonthName = "julho";
-    return getMonthName;
-  }
-  if (dataMes === 7) {
-    const getMonthName = "agosto";
-    return getMonthName;
-  }
-  if (dataMes === 8) {
-    const getMonthName = "setembro";
-    return getMonthName;
-  }
-  if (dataMes === 9) {
-    const getMonthName = "outubro";
-    return getMonthName;
-  }
-  if (dataMes === 10) {
-    const getMonthName = "novembro";
-    return getMonthName;
-  }
-  if (dataMes === 11) {
-    const getMonthName = "dezembro";
-    return getMonthName;
-  }
+  const nomesMeses = [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro",
+  ];
+  return nomesMeses[dataMes];
 }
