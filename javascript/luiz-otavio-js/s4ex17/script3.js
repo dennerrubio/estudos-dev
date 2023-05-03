acce5323bb31c738f3490333733379ba30c89a9f
Denner Rubio <denner.psd@gmail.com>
2023-05-03 17:46:12 -0300

    js s4 ex 17 att

---
+++ javascript/luiz-otavio-js/s4ex17/script3.js
@@ -0,0 +1,18 @@
+(function () {
+  const frutas = [
+    "Maça",
+    "Pera",
+    "Abacaxi",
+    "Uva",
+    "Tomate",
+    "Melancia",
+    "Melão",
+  ];
+  function listaFrutas() {
+    for (let i of frutas) {
+      console.log(i);
+    }
+  }
+
+  listaFrutas();
+})();
