126c521b15491adbfb4a1c536bd156e2853e0be6
Denner Rubio <denner.psd@gmail.com>
2023-05-18 17:59:35 -0300

    js s6 ex23 add

---
+++ javascript/luiz-otavio-js/s6ex23/script.js
@@ -0,0 +1,26 @@
+function Dados(nome, sobrenome, idade, id) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+  this.idNum = id;
+
+  Object.defineProperty(this, "id", {
+    enumerable: true,
+    configurable: true,
+    get: function () {
+      return idNum;
+    },
+    set: function (valor) {
+      if (typeof valor !== "number") {
+        console.log("Isto não é um número");
+        return;
+      }
+      idNum = valor;
+    },
+  });
+}
+
+const pessoa1 = new Dados("Denner", "Rubio", "26", 7);
+const pessoa2 = new Dados("César", "Rubio", "6", "13");
+pessoa1.id = "4";
+console.log(pessoa1.id);
