3dd8f866c963348f8af38eb8200d47036591df08
Denner Rubio <denner.psd@gmail.com>
2023-05-24 07:00:33 -0300

    js s6 ex26 att

--- javascript/luiz-otavio-js/s6ex26/script.js
+++ javascript/luiz-otavio-js/s6ex26/script.js
@@ -6,20 +6,43 @@ botao.addEventListener("click", function (e) {
   const cpfArray = Array.from(cpf).slice(0, -2);
   console.log(cpfArray);
   if (cpf.length > 11 || cpf.length < 11) {
-    console.log("cpf invalido");
+    alert("CPF INVALIDO");
   } else {
     calculo1(cpfArray);
-    console.log(cpfArray);
+    calculo2(cpfArray);
+    const somaArray = cpfArray.reduce((a, b) => a + b);
+    if (somaArray === cpf) {
+      alert("SEU CPF É VÁLIDO");
+    } else {
+      alert("ESSE CPF NÃO EXISTE");
+    }
   }
 });
 
 function calculo1(cpfArray) {
-  console.log("cpf valido");
   let calc = 0;
   for (let i = 0; i < 9; i++) {
     calc += cpfArray[i] * (10 - i);
     console.log(calc);
   }
   const res1Dig = 11 - (calc % 11);
-  cpfArray.push(String(res1Dig));
+  if (res1Dig > 9) {
+    cpfArray.push("0");
+  } else {
+    cpfArray.push(String(res1Dig));
+  }
+}
+
+function calculo2(cpfArray) {
+  let calc2 = 0;
+  for (let i = 0; i < 10; i++) {
+    calc2 += cpfArray[i] * (11 - i);
+    console.log(calc2);
+  }
+  const res2Dig = 11 - (calc2 % 11);
+  if (res2Dig > 9) {
+    cpfArray.push("0");
+  } else {
+  cpfArray.push(String(res2Dig));
+  }
 }
