7f6e7cd026d414456748ff0b6609845fe212f739
Denner Rubio <denner.psd@gmail.com>
2023-05-30 17:58:26 -0300

    js s7 ex32 att

---
+++ javascript/luiz-otavio-js/s7ex32/js/form.js
@@ -0,0 +1,127 @@
+class Formulario {
+  constructor() {
+    this.formulario = document.querySelector(".formulario");
+    this.iniciar();
+  }
+
+  iniciar() {
+    this.formulario.addEventListener("submit", (e) => {
+      this.handleSubmit(e);
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.isInfoValid();
+    console.log(validar);
+  }
+
+  isInfoValid() {
+    this.validar = true;
+
+    this.clearError();
+    this.validarNome();
+    this.validarSobrenome();
+    this.validarCPF();
+    this.validarUser();
+    this.validarSenha();
+  }
+
+  validarNome() {
+    const nome = document.querySelector("#inputnome");
+    if (nome.value === "") {
+      this.createError(`O campo "nome" não pode estar em branco.`, ".divnome");
+      this.validar = false;
+    }
+  }
+
+  validarSobrenome() {
+    const sobrenome = document.querySelector("#inputsobrenome");
+    if (sobrenome.value === "") {
+      this.createError(
+        `O campo "sobrenome" não pode estar em branco.`,
+        ".divsobrenome"
+      );
+      this.validar = false;
+    }
+  }
+
+  validarCPF() {
+    const cpf = document.querySelector("#inputcpf");
+    const cpf1 = new CPF(cpf.value);
+    if (cpf1.resultado === true) {
+      this.createError(`eita.`, ".divcpf");
+      this.validar = false;
+    }
+  }
+
+  validarUser() {
+    const usuario = document.querySelector("#inputusuario");
+    if (usuario.value === "") {
+      this.createError(
+        `O campo "usuário" não pode estar em branco.`,
+        ".divusuario"
+      );
+      this.validar = false;
+    } else if (!usuario.value.match(/^[a-zA-z0-9]+$/g)) {
+      this.createError(`Apenas letras e números.`, ".divusuario");
+      this.validar = false;
+    } else if (usuario.value.length < 3 || usuario.value.length > 12) {
+      this.createError(
+        `O campo "usuário" não tem a quantidade correta de digitos.`,
+        ".divusuario"
+      );
+      this.validar = false;
+    }
+  }
+
+  validarSenha() {
+    const senha = document.querySelector("#inputsenha");
+    const senhaconfirm = document.querySelector("#inputsenha-valid");
+
+    if (senha.value === "") {
+      this.createError(
+        `O campo "senha" não pode estar em branco.`,
+        ".divsenha"
+      );
+      this.validar = false;
+    } else if (senha.value.length < 6 || senha.value.length > 12) {
+      this.createError(
+        `O campo "senha" não tem a quantidade correta de digitos.`,
+        ".divsenha"
+      );
+      this.validar = false;
+    }
+
+    if (senhaconfirm.value === "") {
+      this.createError(
+        `O campo de confirmação de senha não pode estar em branco.`,
+        ".divsenha-valid"
+      );
+      this.validar = false;
+    }
+
+    if (senhaconfirm.value !== senha.value) {
+      this.createError(`As senhas não são iguais.`, ".divsenha-valid");
+      this.validar = false;
+    }
+  }
+
+  createError(msg, selector) {
+    const texto = document.createElement("p");
+    texto.innerHTML = msg;
+    texto.classList.add("erro");
+    document.querySelector(selector).appendChild(texto);
+  }
+
+  clearError() {
+    document.querySelector(".divnome").innerHTML = "";
+    document.querySelector(".divsobrenome").innerHTML = "";
+    document.querySelector(".divcpf").innerHTML = "";
+    document.querySelector(".divusuario").innerHTML = "";
+    document.querySelector(".divsenha").innerHTML = "";
+    document.querySelector(".divsenha-valid").innerHTML = "";
+  }
+}
+
+const validar = new Formulario();
---
+++ javascript/luiz-otavio-js/s7ex32/js/validarcpf.js
@@ -0,0 +1,46 @@
+class CPF {
+  constructor(cpfInicial) {
+    this.cpfInicial = cpfInicial;
+    this.cpfLimpo = "";
+    this.cpfArray = "";
+    this.resultado = false;
+    this.validarCPF();
+  }
+
+  validarCPF() {
+    this.limparCPF();
+    if (this.cpfLimpo.length > 11 || this.cpfLimpo.length < 11) {
+      return console.log("CPF Inválido");
+    }
+    this.calculo(0);
+    this.calculo(1);
+    this.comparativo();
+  }
+
+  limparCPF() {
+    this.cpfLimpo = this.cpfInicial.replace(/\D+/g, "");
+    this.cpfArray = Array.from(this.cpfLimpo).slice(0, -2);
+  }
+
+  calculo(value) {
+    let calc = 0;
+    for (let i = 0; i < 9 + value; i++) {
+      calc += this.cpfArray[i] * (10 + value - i);
+    }
+    const digito = 11 - (calc % 11);
+    if (digito > 9) {
+      this.cpfArray.push("0");
+    } else {
+      this.cpfArray.push(String(digito));
+    }
+  }
+
+  comparativo() {
+    const somaArray = this.cpfArray.reduce((a, b) => a + b);
+    if (somaArray === this.cpfLimpo) {
+      return (this.resultado = true);
+    } else {
+      return console.log("Este CPF não existe.");
+    }
+  }
+}
