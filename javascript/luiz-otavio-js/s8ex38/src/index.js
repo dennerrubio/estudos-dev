356a9f08f9c4401a8702f87f221af76e0e801c30
Denner Rubio <denner.psd@gmail.com>
2023-06-12 13:08:03 -0300

    js s8 ex38 add

---
+++ javascript/luiz-otavio-js/s8ex38/src/index.js
@@ -0,0 +1,5 @@
+const nome = "Denner"
+import { sobrenome } from "./modulo"
+const idade = 26
+
+console.log(nome, sobrenome, idade)
---
+++ javascript/luiz-otavio-js/s8ex38/src/modulo.js
@@ -0,0 +1,3 @@
+const sobrenome = "Rubio"
+
+export { sobrenome }
