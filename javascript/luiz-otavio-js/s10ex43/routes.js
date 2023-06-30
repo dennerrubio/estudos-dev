a8d3ec4f70233eb681fa5966e69657354de927b0
Denner Rubio <denner.psd@gmail.com>
2023-06-30 16:59:01 -0300

    js s10 ex43 att

--- javascript/luiz-otavio-js/s10ex43/routes.js
+++ javascript/luiz-otavio-js/s10ex43/routes.js
@@ -3,8 +3,14 @@ const route = express.Router();
 const homeControl = require("./src/controllers/homeControl");
 const contatoControl = require("./src/controllers/contatoControl");
 
-route.get("/", homeControl.paginainicial);
+route.get("/", middle, homeControl.paginainicial);
 
-route.get("/contato/:iduser?", contatoControl.paginainicial);
+route.get("/contato/:iduser?", middle, contatoControl.paginainicial);
+
+function middle(req, res, next) {
+    console.log("Eu passei por aqui");
+    next();
+  };
+  
 
 module.exports = route;
