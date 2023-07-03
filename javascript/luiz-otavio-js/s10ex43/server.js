3c5657b7f4ad79dbe50c9e3223465afb1309ae38
Denner Rubio <denner.psd@gmail.com>
2023-07-03 14:36:54 -0300

    js s10 ex43 att

--- javascript/luiz-otavio-js/s10ex43/server.js
+++ javascript/luiz-otavio-js/s10ex43/server.js
@@ -1,5 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conectado com sucesso.");
+    app.emit("Pronto");
+  })
+  .catch((e) => console.log(e));
 const route = require("./routes");
 const path = require("path");
 app.use(express.static(path.resolve(__dirname, "public")));
@@ -7,7 +19,9 @@ app.use(route);
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
-app.listen(3000, () => {
-  console.log("Servidor funcionando.");
-  console.log("http://localhost:3000");
+app.on("Pronto", () => {
+  app.listen(3000, () => {
+    console.log("Servidor funcionando.");
+    console.log("http://localhost:3000");
+  });
 });
