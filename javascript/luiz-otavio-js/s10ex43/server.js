335805316d064ce58a5fa51e702aceb8ba28396d
Denner Rubio <denner.psd@gmail.com>
2023-07-04 06:46:44 -0300

    js s10 ex43 att

--- javascript/luiz-otavio-js/s10ex43/server.js
+++ javascript/luiz-otavio-js/s10ex43/server.js
@@ -12,10 +12,23 @@ mongoose
     app.emit("Pronto");
   })
   .catch((e) => console.log(e));
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
 const route = require("./routes");
 const path = require("path");
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(route);
+const sessionOptions = session({
+  secret: "its a secret",
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: trye,
+  },
+});
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
